80a6e5a4e90ebb0ddb94d80a3ac2701f446af4d6
Carlos Daniel <cadanielmarchesin@gmail.com>
2025-07-03 16:10:30 -0300

    correção da prova

--- src/main/java/correcao/enem/service/ExamCorrectionService.java
+++ src/main/java/correcao/enem/service/ExamCorrectionService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -16,14 +17,33 @@ public class ExamCorrectionService {
 
     public String correctExam(MultipartFile file, UserAnswers data) {
         String text = extractorPdf.extractContentFromPdf(file);
-        Map<Integer, String> gabarito = extractAnswersFromText(text);
+        Map<Integer, String> gabarito = extractCorrectAnswersFromText(text);
 
-        validateAnswerValues(data.answers());
+        Map<Integer, String> userAnswers = data.answers();
+        validateUserAnswerValues(userAnswers);
 
-        return data.answers().toString();
+        int correctCount = 0;
+        int wrongCount = 0;
+
+        for (Map.Entry<Integer, String> entry : userAnswers.entrySet()) {
+            Integer number = entry.getKey();
+            String userAnswer = entry.getValue();
+
+            if (gabarito.get(number) == null) {
+                continue;
+            }
+
+            if (!gabarito.get(number).equalsIgnoreCase(userAnswer)) {
+                wrongCount++;
+            } else {
+                correctCount++;
+            }
+        }
+
+        return String.format("Errou: %d Acertou: %d", wrongCount, correctCount);
     }
 
-    private Map<Integer, String> extractAnswersFromText(String text) {
+    private Map<Integer, String> extractCorrectAnswersFromText(String text) {
         Map<Integer, String> answers = new LinkedHashMap<>();
         String[] lines = text.split("\\r?\\n");
 
@@ -45,7 +65,7 @@ public class ExamCorrectionService {
         return answers;
     }
 
-    private void validateAnswerValues(Map<Integer, String> answers) {
+    private void validateUserAnswerValues(Map<Integer, String> answers) {
         for (Map.Entry<Integer, String> entry : answers.entrySet()) {
             String value = entry.getValue();
             if (!value.matches("(?i)[a-e]")) {
