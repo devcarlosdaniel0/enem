6dc8f1b86b08ef472cabd8b23a14310ece57ce8c
Carlos Daniel <cadanielmarchesin@gmail.com>
2025-07-15 18:35:05 -0300

    arrumando UserAnswersRequest para answers notNull

--- src/main/java/correcao/enem/dto/UserAnswersRequest.java
+++ src/main/java/correcao/enem/dto/UserAnswersRequest.java
@@ -6,12 +6,10 @@ import jakarta.validation.constraints.*;
 import java.util.Map;
 
 public record UserAnswersRequest(LanguageOption languageOption,
-                                 Map<@NotNull @Max(value = 180) @Min(value = 1) Integer,
+                                 @NotNull Map<@NotNull @Max(value = 180) @Min(value = 1) Integer,
                 @NotBlank @Pattern(regexp = "(?i)[a-e]", message = "Answers must be only [A,B,C,D,E]") String> answers) {
 
     public UserAnswersRequest {
-        if (answers != null) {
-            answers.replaceAll((key, value) -> value != null ? value.trim().toUpperCase() : null);
-        }
+        answers.replaceAll((key, value) -> value != null ? value.trim().toUpperCase() : null);
     }
 }
