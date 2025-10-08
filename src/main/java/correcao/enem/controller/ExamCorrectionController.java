d9716554977f85ea43cd27ee3e4b507758d2ba16
Carlos Daniel <cadanielmarchesin@gmail.com>
2025-10-08 08:49:59 -0300

    CrossOrigin *

--- src/main/java/correcao/enem/controller/ExamCorrectionController.java
+++ src/main/java/correcao/enem/controller/ExamCorrectionController.java
@@ -5,17 +5,17 @@ import correcao.enem.dto.UserAnswersRequest;
 import correcao.enem.service.ExamCorrectionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/correct-exam")
 @RequiredArgsConstructor
+@Slf4j
 public class ExamCorrectionController {
 
     private final ExamCorrectionService examCorrectionService;
@@ -24,6 +24,8 @@ public class ExamCorrectionController {
     public ResponseEntity<ResultResponse> correctExam(
             @RequestPart("file") MultipartFile filePath,
             @RequestPart("userAnswers") @Valid UserAnswersRequest userAnswersRequest) {
+
+        log.info(String.valueOf(userAnswersRequest));
 
         return ResponseEntity.ok(examCorrectionService.correctExam(filePath, userAnswersRequest));
     }
