03d715c77550861a907636245948ff01e7632694
Carlos Daniel <cadanielmarchesin@gmail.com>
2025-11-25 15:54:19 -0300

    Update UserAnswersRequest.java

--- src/main/java/correcao/enem/dto/UserAnswersRequest.java
+++ src/main/java/correcao/enem/dto/UserAnswersRequest.java
@@ -6,8 +6,7 @@ import jakarta.validation.constraints.*;
 import java.util.Map;
 
 public record UserAnswersRequest(LanguageOption languageOption,
-                                 @NotEmpty
-                                 Map<@NotNull @Max(value = 180) @Min(value = 1) Integer,
+                                 @NotEmpty Map<@NotNull @Min(value = 1) @Max(value = 180) Integer,
                 @NotBlank @Pattern(regexp = "(?i)[a-e]", message = "Answers must be only [A,B,C,D,E]") String> answers) {
 
     public UserAnswersRequest {
