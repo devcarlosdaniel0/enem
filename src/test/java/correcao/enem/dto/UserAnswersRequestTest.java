4d5c4c2386e5178822529743a7ef8c1a44808940
Carlos Daniel <cadanielmarchesin@gmail.com>
2025-12-12 11:38:02 -0300

    unit tests for user answer request

--- src/test/java/correcao/enem/dto/UserAnswersRequestTest.java
+++ src/test/java/correcao/enem/dto/UserAnswersRequestTest.java
@@ -1,15 +1,16 @@
 package correcao.enem.dto;
 
 import correcao.enem.enums.LanguageOption;
+import correcao.enem.exceptions.InvalidYearException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 class UserAnswersRequestTest {
     @Test
@@ -52,5 +53,58 @@ class UserAnswersRequestTest {
         assertFalse(regEx.matcher(userAnswersRequest.answers().get(2)).matches());
         assertFalse(regEx.matcher(userAnswersRequest.answers().get(3)).matches());
         assertFalse(regEx.matcher(userAnswersRequest.answers().get(4)).matches());
+    }
+
+    @Test
+    @DisplayName("Should throw exception when manual exam year is greater than actual year with tolerance of 1")
+    void shouldThrowExceptionWhenManualExamYearIsGreaterThanActualYearWithToleranceOf1() {
+        // Arrange
+        Map<Integer, String> answers = new HashMap<>(Map.of(
+                1, "A",
+                2, "B",
+                3, "C",
+                4, "C"));
+
+        int actualYear = LocalDateTime.now().getYear();
+
+        int invalidYear = actualYear + 2;
+
+        // Act && Assert
+        assertThrows(InvalidYearException.class,
+                () -> new UserAnswersRequest(null, invalidYear, answers));
+    }
+
+    @Test
+    @DisplayName("Should proceed successfully when manual exam year is with tolerance of 1")
+    void shouldProceedSuccessfullyWhenManualExamYearIsWithToleranceOf1() {
+        // Arrange
+        Map<Integer, String> answers = new HashMap<>(Map.of(
+                1, "A",
+                2, "B",
+                3, "C",
+                4, "C"));
+
+        int actualYear = LocalDateTime.now().getYear();
+
+        int toleranceOf1Year = actualYear + 1;
+
+        // Act && Assert
+        assertDoesNotThrow(() -> new UserAnswersRequest(null, toleranceOf1Year, answers));
+    }
+
+    @Test
+    @DisplayName("Should proceed successfully when manual exam year is actual year")
+    void shouldProceedSuccessfullyWhenManualExamYearIsActualYear() {
+        // Arrange
+        Map<Integer, String> answers = new HashMap<>(Map.of(
+                1, "A",
+                2, "B",
+                3, "C",
+                4, "C"));
+
+        int actualYear = LocalDateTime.now().getYear();
+
+        // Act && Assert
+        assertDoesNotThrow(() -> new UserAnswersRequest(null, actualYear, answers));
     }
 }
