0bd457eaf4d625ac44e95f83861ce654954a6c87
Carlos Daniel <cadanielmarchesin@gmail.com>
2025-12-16 12:39:47 -0300

    Revert "NotBlank UserRequest"
    This reverts commit d8c1f63c95956cb5552f9e8ea2a509b66faa19ee.

--- src/main/java/correcao/enem/dto/UserAnswersRequest.java
+++ src/main/java/correcao/enem/dto/UserAnswersRequest.java
@@ -9,7 +9,7 @@ import java.util.Map;
 
 public record UserAnswersRequest(LanguageOption languageOption,
                                  Integer manualExamYear,
-                                 @NotBlank Map<@Min(value = 1) @Max(value = 180) Integer,
+                                 @NotNull Map<@Min(value = 1) @Max(value = 180) Integer,
                                          @Pattern(regexp = "(?i)[a-e]") String> answers) {
 
     public UserAnswersRequest {
